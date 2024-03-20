cd9787ef827f88bbf7384cf65226c135e43e9880
huoyanru123 <1259084194@qq.com>
2024-03-20 21:41:32 +0800

    test

--- Assets/Scenes/as1.cs
+++ Assets/Scenes/as1.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AAA : MonoBehaviour
+public class as1 : MonoBehaviour
 {
+     private AAA x;
     // Start is called before the first frame update
     void Start()
     {
-        
+    x.fun1();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    public void fun1()
-    {
-      Debug.Log("Hello World!");
     }
 }
