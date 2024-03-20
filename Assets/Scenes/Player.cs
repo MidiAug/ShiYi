24bbd70b7fecad3d60b532ce7e0c053859349a2a
MidiAug <2234649224@qq.com>
2024-03-20 22:28:07 +0800

    111

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AAA : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    Player player;
+    private int level; // 人物等级
+    private float hp; // 人物生命
+    private float curTime; // 当前时间
+
     // Start is called before the first frame update
     void Start()
     {
-        int a = player.getLevel();
+        
     }
 
     // Update is called once per frame
@@ -16,8 +19,8 @@ public class AAA : MonoBehaviour
     {
         
     }
-    public void fun1()
-    {
-
+    // 获取人物等
+    public int getLevel()
+    {// 获取人物等级
+        return level; }
     }
-}
