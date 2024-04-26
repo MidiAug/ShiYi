7828a355ab7775c05916e4bf52147ea303f485fc
i1111 <1259084194@qq.com>
2024-04-26 18:23:02 +0800

    修正了闪现的逻辑
    1

--- Assets/Playertest/Playercontroller.cs
+++ Assets/Playertest/Playercontroller.cs
@@ -38,28 +38,44 @@ public class Playercontroller : MonoBehaviour
     }
 
   private void FixedUpdate()
-  { // 闪现距离
-    float dashDist = 5f;
-    Vector3 dashPosition = transform.position + new Vector3(dirX, 0, 0) * dashDist;
+  {
+    float dashDist = 5f;  // 闪现距离
+    Vector2 dashDirection = new Vector2(dirX, 0).normalized;
+    Vector2 offset = dashDirection * 0.1f;  // 在玩家前方稍作偏移
+    Vector2 startPosition = new Vector2(transform.position.x,transform.position.y) + offset;  // 调整射线起始位置
+    Vector2 dashPosition = startPosition + dashDirection * dashDist;
+
     if (isDash)
     {
+      // 使用LayerMask来忽略玩家自身的层
+      int layerMask = 1 << LayerMask.NameToLayer("player");
+      layerMask = ~layerMask;  // 反转LayerMask，忽略"Player"层
+
+      RaycastHit2D hit = Physics2D.Raycast(startPosition, dashDirection, dashDist, layerMask);
+      if (hit.collider != null)  // 如果射线投射检测到了障碍物
+      {
+        dashPosition = hit.point;  // 将dash终点设置为碰撞点位置
+        dashPosition -= dashDirection * 0.1f;  // 留出一点空间，避免卡在障碍物上
+      }
+
       rb.MovePosition(dashPosition);
       isDash = false;
     }
   }
-    Vector2 position;
-    private bool IsBack = false;
-    private void Back()
-    {
-        if(IsBack==false)
-        {
-            position = gameObject.transform.position;
-            IsBack = true;
-        }
-        else
-        {
-            gameObject.transform.position = position;
-            IsBack = false;
-        }
-    }
+  
+  Vector2 position;
+  private bool IsBack = false;
+  private void Back()
+  {
+      if(IsBack==false)
+      {
+          position = gameObject.transform.position;
+          IsBack = true;
+      }
+      else
+      {
+          gameObject.transform.position = position;
+          IsBack = false;
+      }
+  }
 }
