cd745bf5f5417f1c1308385e4099576bd184de78
i1111 <1259084194@qq.com>
2024-05-07 21:01:58 +0800

    补充陷阱&修改电梯
    # 补充陷阱
    - 为第二部分地图铺设了陷阱
    # 修改电梯
    - 为电梯加设了三个属性
      - desireTime：电梯在线性运动下到达目的地所需时间，据此重新计算速度
      - chargeTime：电梯在线性运动下达到其最大速度所需时间
      - isBackAndForth：判断电梯是否来回运动，如不是则采取线性运动的策略(匀速)
    - 为电梯增加了线性运动的情况

--- Assets/Map/Scripts/ElevatorCtrl.cs
+++ Assets/Map/Scripts/ElevatorCtrl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Net;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ElevatorCtrl : MonoBehaviour
@@ -10,14 +11,18 @@ public class ElevatorCtrl : MonoBehaviour
     // 将switch合并一起方便管理
     public enum MoveDir { GO, BACK } // 物体在两点间的方向
 
-    public bool isMove = false; // 
+    public bool isMove = false; 
+    public bool isBackAndForth=false;
+    public float chargeTime = 1f;    // 达到最大速度所需时间
+    public float desireTime = 1.5f;  // 期望到达目标地点的时间
     public float speed;
 
     public GameObject board;
     public GameObject endObj;
     private Vector3 startPos;
     private Vector3 endPos;
-    private MoveDir moveDir; 
+    private MoveDir moveDir;
+
     private float deltaTime = 0;
 
     void Start()
@@ -27,10 +32,22 @@ public class ElevatorCtrl : MonoBehaviour
     }
     void Update()
     {
-        if (isMove)
+        // 如果不来回运动且电梯已到终点，停止运动
+        if (board.transform.position == endPos && !isBackAndForth)
+          isMove = false;
+        if (isMove&&isBackAndForth)
         {
             deltaTime += Time.deltaTime;
             float t = (Mathf.Sin((deltaTime) * speed - Mathf.PI / 2) + 1f) / 2f;
+            board.transform.position = Vector3.Lerp(startPos, endPos, t);
+        }
+        else if (isMove&&!isBackAndForth) {
+            deltaTime += Time.deltaTime;
+            // 计算从起点到终点的总距离
+            float totalDistance = Vector3.Distance(startPos, endPos);
+            speed = totalDistance / desireTime * Mathf.Min(deltaTime / chargeTime,1);
+            float t = Mathf.Min(deltaTime * speed / totalDistance, 1f);
+            // 根据 t 设置位置
             board.transform.position = Vector3.Lerp(startPos, endPos, t);
         }
 
